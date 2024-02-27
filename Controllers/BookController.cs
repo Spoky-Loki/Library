864cbe7f20291e76a8663ef59692344cf0ff2659
Viktor <viter02@inbox.ru>
2024-02-27 13:28:03 +0300

    Add Model, View and Controller for Book

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,89 +1,120 @@
-﻿using System;
+﻿using Library.Models;
+using Npgsql;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace Library.Controllers
 {
     public class BookController : Controller
     {
+        private NpgsqlConnection connection;
+        private NpgsqlCommand command;
+
+        public BookController()
+        {
+            connection = new NpgsqlConnection(
+                connectionString: WebConfigurationManager.ConnectionStrings["DbContext"].ConnectionString);
+
+            command = new NpgsqlCommand();
+            command.Connection = connection;
+        }
+
         [HttpGet]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            command.Connection.Open();
+
+            command.CommandText = $"SELECT * FROM Books";
+            NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+            var books = new List<BookModel>();
+            while (await reader.ReadAsync())
+            {
+                books.Add(new BookModel(
+                    (int)reader["id"],
+                    reader["title"] as string,
+                    reader["author"] as string,
+                    reader["description"] as string,
+                    (int)reader["count"]));
+            }
+
+            return View(books);
         }
 
-        // GET: Book/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Book/Create
+        [HttpGet]
         public ActionResult Create()
         {
+            return View(new BookModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(BookModel bookModel)
+        {
+            command.Connection.Open();
+
+            command.CommandText = $"INSERT INTO Books (title, author, description, count) VALUES (@title, @author, @description, @count)";
+            command.Parameters.AddWithValue("@title", bookModel.Title);
+            command.Parameters.AddWithValue("@author", bookModel.Author);
+            command.Parameters.AddWithValue("@description", bookModel.Description);
+            command.Parameters.AddWithValue("@count", bookModel.Count);
+            await command.ExecuteNonQueryAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int id)
+        {
+            command.Connection.Open();
+
+            command.CommandText = $"SELECT * FROM Books WHERE id = {id}";
+            NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+            BookModel book = new BookModel();
+            while (await reader.ReadAsync())
+            {
+                book = new BookModel(
+                    (int)reader["id"],
+                    reader["title"] as string,
+                    reader["author"] as string,
+                    reader["description"] as string,
+                    (int)reader["count"]);
+            }
+
+            if (book.Title != null)
+                return View(book);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, BookModel bookModel)
+        {
+            command.Connection.Open();
+
+            command.CommandText = $"UPDATE Books " +
+                $"SET title='{bookModel.Title}'," +
+                $"author='{bookModel.Author}'," +
+                $"description='{bookModel.Description}'," +
+                $"count='{bookModel.Count}' " +
+                $"WHERE id={id}";
+            await command.ExecuteNonQueryAsync();
+
             return View();
         }
 
-        // POST: Book/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            command.Connection.Open();
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+            command.CommandText = $"DELETE FROM Books WHERE id={id}";
+            await command.ExecuteNonQueryAsync();
 
-        // GET: Book/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Book/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Book/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Book/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Models/BookModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Название")]
+        public string Title { get; set; }
+
+        [DisplayName("Автор")]
+        public string Author { get; set; }
+
+        [DisplayName("Описание")]
+        public string Description { get; set; }
+
+        [DisplayName("Колличестов")]
+        public int Count { get; set; }
+
+        public BookModel() { }
+
+        public BookModel(int id, string title, string author, string description, int count)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Description = description;
+            Count = count;
+        }
+    }
+}
